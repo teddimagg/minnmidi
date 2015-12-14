a1901fe744ece8dc0418ad2e0f1aa361eca9a60b
teddimagg <theodor@miklihvellur.is>
2015-12-14 06:42:02 +0000

    loginogregister

--- client/shared/nav/nav.js
+++ client/shared/nav/nav.js
@@ -1,11 +1,14 @@
 Session.set('login', false);
+Session.set('register', false);
 
 Template.nav.events({
 	'click .navprofile': function(event){
-		var smegma = Session.get('login');
-		if(smegma)
+		var log = Session.get('login');
+		var reg = Session.get('register');
+		if(log || reg)
 		{
 			Session.set('login', false);
+			Session.set('register', false);
 		}
 		else
 		{
@@ -17,5 +20,40 @@ Template.nav.events({
 Template.nav.helpers({
 	'isLogin': function(){
 		return Session.get('login');
+	}
+});
+
+Template.nav.helpers({
+	'isRegister': function(){
+		return Session.get('register');
+	}
+});
+
+Template.navLogin.events({
+	'click .regbtn': function(event){
+		Session.set('register', true);
+		Session.set('login', false);
+	},
+	'click .loginbtn': function(event){
+		event.preventDefault();
+		var emailVar = $('[name=email]').val();
+		var passwordVar = $('[name=pass]').val();
+		Meteor.loginWithPassword(emailVar, passwordVar);
+	},
+	'click .logoutbtn': function(event){
+		event.preventDefault();
+		Meteor.logout();
+	}
+});
+
+Template.navRegister.events({
+	'submit form': function(event){
+		event.preventDefault();
+		var emailVar = $('[name=email]').val();
+		var passwordVar = $('[name=pass]').val();
+		Accounts.createUser({
+			email: emailVar,
+			password: passwordVar
+		});
 	}
 });
