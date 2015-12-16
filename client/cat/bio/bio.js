1e65a18adbf526248ba95fe870e362672a3bc5e7
teddimagg <theodor@miklihvellur.is>
2015-12-16 10:25:55 +0000

    meira sortpanel og virkni

--- client/cat/bio/bio.js
+++ client/cat/bio/bio.js
@@ -1,8 +1,44 @@
+Session.set('genreView', true);
+Session.set('sortType', 1);
+
+if($(window).width() < 800)
+{
+	Session.set('genreView', false);
+}
+
+Template.nav.onRendered(function() {
+  $(window).resize(function() {
+  	if($(window).width() < 800)
+  	{
+  		Session.set('genreView', false);
+  	}
+  	else
+  	{
+  		Session.set('genreView', true);
+  	}
+  });
+});
+
 Template.bio.helpers({
 	'movieEvent': function()
 	{
-		return movieEvents.find().fetch();
-	},
+		var sort = Session.get('sortType');
+		var ret;
+		switch(sort){
+			case 0:
+				ret = movieEvents.find({},{sort:{nafn: 1}});
+				break;
+			case 1:
+				ret = movieEvents.find().fetch();
+				break;
+			case 2:
+				sortString = "title: 1";
+		}
+		return ret;
+	}
+});
+
+Template.filter.helpers({
 	'getWeek': function()
 	{
 		var today = moment();
@@ -31,4 +67,40 @@ Template.bio.helpers({
 		
 		return week;
 	},
+	'isGenreView': function()
+	{
+		return Session.get('genreView');
+	}
+
 });
+
+Template.filter.events({
+	'click .byname': function(event){
+		Session.set('sortType', 0);
+		remSortClasses();
+		$(".byname").addClass("active");
+		$(".bydate").addClass("inactive");
+		$(".byaird").addClass("inactive");
+
+	},
+	'click .bydate': function(event){
+		Session.set('sortType', 1);
+		remSortClasses();
+		$(".byname").addClass("inactive");
+		$(".bydate").addClass("active");
+		$(".byaird").addClass("inactive");
+	},
+	'click .byaird': function(event){
+		Session.set('sortType', 2);
+		remSortClasses();
+		$(".byname").addClass("inactive");
+		$(".bydate").addClass("inactive");
+		$(".byaird").addClass("active");
+	}
+});
+
+function remSortClasses(){
+	$(".byname").removeClass("active inactive");
+	$(".bydate").removeClass("active inactive");
+	$(".byaird").removeClass("active inactive");
+}
